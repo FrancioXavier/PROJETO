8978799250863c381d3a8788f7eaf860b90c7d0b
FrancioXavier <francioxaviersilva@gmail.com>
2022-05-03 15:30:05 -0300

    Models & ORM final

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -6,9 +6,10 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -29,10 +30,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
