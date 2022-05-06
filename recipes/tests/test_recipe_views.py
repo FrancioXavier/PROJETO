2cbacb3d98540bbc10d7ab1c08d14ad3ef334eed
FrancioXavier <francioxaviersilva@gmail.com>
2022-05-06 13:16:48 -0300

    fixtures

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipesViewsTest(TestCase):
@@ -22,6 +23,38 @@ class RecipesViewsTest(TestCase):
             'No recipes found here;-;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_templates_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='user@name.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe title',
+            description='Recipe description',
+            slug='recipe-slug',
+            preparation_time='10',
+            preparation_time_unit='minutos',
+            servings='10',
+            servings_unit='porções',
+            preparation_steps='Recipe steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipe = response.context['recipes']
+
+        self.assertIn('Recipe title', content)
+        self.assertIn('10 minutos', content)
+        self.assertIn('10 porções', content)
+        self.assertEqual(len(response_context_recipe), 1)
 
     def test_recipe_category_view_return_404_if_no_recipes_found(self):
         response = self.client.get(
