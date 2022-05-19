d85d1e0c6411f33b6482e25a3837f9b99b7856c8
FrancioXavier <francioxaviersilva@gmail.com>
2022-05-19 15:47:47 -0300

    tests pagination with mock

--- recipes/tests/test_recipe_view_home.py
+++ recipes/tests/test_recipe_view_home.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_recipe_base import RecipeTestBase
@@ -42,3 +44,18 @@ class RecipesHomeViewsTest(RecipeTestBase):
             'No recipes found here;-;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'r{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
--- recipes/tests/test_recipe_view_search.py
+++ recipes/tests/test_recipe_view_search.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_recipe_base import RecipeTestBase
@@ -77,3 +79,22 @@ class RecipesSearchViewsTest(RecipeTestBase):
 
         self.assertIn(recipe1, response_both.context['recipes'])
         self.assertIn(recipe2, response_both.context['recipes'])
+
+    @patch('recipes.views.PER_PAGE', new=3)
+    def test_recipe_search_is_paginated(self):
+        title = 'Recipe Title'
+        for i in range(8):
+            kwargs = {'slug': f'r{i}',
+                      'author_data': {'username': f'r{i}'},
+                      'title': {title}}
+            self.make_recipe(**kwargs)
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title}')
+        recipes = response1.context['recipes']
+        paginator = recipes.paginator
+
+        self.assertEqual(paginator.num_pages, 3)
+        self.assertEqual(len(paginator.get_page(1)), 3)
+        self.assertEqual(len(paginator.get_page(2)), 3)
+        self.assertEqual(len(paginator.get_page(3)), 2)
